693e6522f80d381981a3f5498483b0ae13f5eccb
DREAMWORLD <chintanbhanderi.logictech@gmail.com>
2024-05-08 12:24:37 +0530

    Firestore added

--- RemoteAdsSdk/build.gradle.kts
+++ RemoteAdsSdk/build.gradle.kts
@@ -45,7 +45,7 @@ afterEvaluate {
                 from (components["release"])
                 groupId = "com.github.chintanbhanderi107"
                 artifactId = "SDK-de-anuncios-remotos"
-                version = "1.1.5"
+                version = "1.1.7"
             }
         }
     }
